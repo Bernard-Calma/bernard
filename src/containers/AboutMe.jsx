9cbeb5337b2ec9991db3de7d0f9359bd15e4d9cb
Bernard-Calma <nard.calma@gmail.com>
2023-01-25 19:40:47 -0800

    font styles imported and set h1 h2 and p fonts

--- src/containers/AboutMe.jsx
+++ src/containers/AboutMe.jsx
@@ -3,7 +3,7 @@ const AboutMe = () => {
         <section className="sectionAboutMe">
             <div className="aboutMe">
                 <h1 className = "aboutMeName">Hi! I'm Bernard Calma</h1>
-                <h3 className = "aboutMeOccupation">I'm a Software Engineer</h3>
+                <h2 className = "aboutMeOccupation">I'm a Software Engineer</h2>
                 <p className="introduction">Highly motivated technology professional with more than 10 years of experience in tech and more than 1 year of experience with Full-Stack Development. Experience completing hardware and software troubleshooting, analysis and resolution procedures. Skilled in Six Sigma, Cloud Computing, Automation and Independent/Team Project Management.</p>
             </div>
             <img className="personalImage" src="images/ProfileImage.jpg" alt="Personal Image"/>
