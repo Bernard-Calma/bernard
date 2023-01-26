c51ed327d8b0a2f3710b78fac6895bed85cf5014
Bernard-Calma <nard.calma@gmail.com>
2023-01-25 19:35:28 -0800

    about me section content added

--- src/containers/AboutMe.jsx
+++ src/containers/AboutMe.jsx
@@ -1,7 +1,12 @@
 const AboutMe = () => {
     return (
         <section className="sectionAboutMe">
-            About Me
+            <div className="aboutMe">
+                <h1 className = "aboutMeName">Hi! I'm Bernard Calma</h1>
+                <h3 className = "aboutMeOccupation">I'm a Software Engineer</h3>
+                <p className="introduction">Highly motivated technology professional with more than 10 years of experience in tech and more than 1 year of experience with Full-Stack Development. Experience completing hardware and software troubleshooting, analysis and resolution procedures. Skilled in Six Sigma, Cloud Computing, Automation and Independent/Team Project Management.</p>
+            </div>
+            <img className="personalImage" src="images/ProfileImage.jpg" alt="Personal Image"/>
         </section>
     )
 }
