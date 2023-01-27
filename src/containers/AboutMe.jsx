0500fb13906a30cd0537429e9c4d2e7740d38e32
Bernard-Calma <nard.calma@gmail.com>
2023-01-27 01:11:29 -0800

    updated introduction

--- src/containers/AboutMe.jsx
+++ src/containers/AboutMe.jsx
@@ -6,7 +6,8 @@ const AboutMe = () => {
             <div className="aboutMe">
                 <h1 className = "aboutMeName">Hi! I'm Bernard Calma</h1>
                 <h2 className = "aboutMeOccupation">I'm a Software Engineer</h2>
-                <p className="introduction">Highly motivated technology professional with more than 10 years of experience in tech and more than 1 year of experience with Full-Stack Development. Experience completing hardware and software troubleshooting, analysis and resolution procedures. Skilled in Six Sigma, Cloud Computing, Automation and Independent/Team Project Management.</p>
+                <p className="introduction">About
+More than 1 year of experience in Full Stack Development (React, Node, Python, Flask, SQL and NoSQL) and 10 years of experience in the IT Industry(Desktop Support, Field Services) in different line of business(Telco, Retail, Media). Expert in hardware/software troubleshooting, analysis, advanced resolution procedures and complex problem solving. Skilled in Six Sigma, Cloud Computing, Team/Project Management and Automation.</p>
             </div>
             <img className="personalImage" src="images/ProfileImage.jpg" alt="Personal Image"/>
         </section>
