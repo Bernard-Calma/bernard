4c77c75a8625394d814dd8a78f03ad1330e88bab
Bernard-Calma <nard.calma@gmail.com>
2023-01-28 15:39:23 -0800

    Experience section created and added to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import AboutMe from './containers/AboutMe';
+import Experience from './containers/Experience';
 
 const App = () => {
   return (
     <div className='App'>
       App
       <AboutMe />
+      <Experience />
     </div>
   )
 }
---
+++ src/containers/Experience.jsx
@@ -0,0 +1,9 @@
+const Experience = () => {
+    return (
+        <section className="sectionExperience" id="idExperience">
+            <h1>Experience</h1>
+        </section>
+    )
+}
+
+export default Experience
