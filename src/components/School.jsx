cf4cc0f9754c99440cf7f54f8f28b9bcd41bab5e
Bernard-Calma <nard.calma@gmail.com>
2023-01-29 15:17:10 -0800

    school components created and styled

--- src/components/School.jsx
+++ src/components/School.jsx
@@ -1,8 +1,15 @@
+import "./School.css"
+
 const School = (props) => {
     return(
         <div className="divSchool">
-            <h2 className="schoolName">{props.school.name}</h2>
-            <img src={props.school.image} alt={props.school.name} />
+            <img className="schoolLogo" src={props.school.image} alt={props.school.name} />
+            <div className="schoolInfo">
+                <h2 className="schoolName">{props.school.name}</h2>
+                <p className="course">{`${props.school.course}(${props.school.degree})`}</p>
+                <p className = "yearGraduated">{props.school.yearGraduate}</p>
+                <p className = "courseDescription">{props.school.description}</p>
+            </div>
         </div>
     )
 }
