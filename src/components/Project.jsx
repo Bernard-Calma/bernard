56b8534ee6b114aaa7294a278df81b2cdc89a73b
nard-calma <nard.calma@gmail.com>
2023-01-30 17:18:08 -0800

    project component created, added and styled

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,12 @@
 const Project = (props) => {
     return(
         <div className="divProject">
-            <h2>{props.project.name}</h2>
+            <h2 className="projectTitle">{props.project.name}</h2>
+            <img className="projectImage" src={props.project.screenshot} alt={props.project.name} />
+            <p className="projectSummary">{props.project.summary}</p>
+            <ul className="skillList">
+                {props.project.skills.map(skill => <li className="skillItem">{skill}</li>)}
+            </ul>
         </div>
     )
 }
