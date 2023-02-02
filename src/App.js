accd90ab400e3c4bd00f48b318fd5d0ea3c97656
nard-calma <nard.calma@gmail.com>
2023-02-01 18:46:38 -0800

    Header container created and added logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Footer from './components/Footer';
 import AboutMe from './containers/AboutMe';
 import Contact from './containers/Contact';
 import Experience from './containers/Experience';
+import Header from './containers/Header';
 import Projects from './containers/Projects';
 
 const App = () => {
   return (
     <div className='App'>
-      App
+      <Header />
       <AboutMe />
       <Experience />
       <Projects />
---
+++ src/containers/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <img 
+                src="/images/TitleLogo.png" 
+                alt="Title Logo"
+                width="100"
+                className="logo"
+            />
+        </header>
+    )
+}
+
+export default Header;
