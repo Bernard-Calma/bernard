4d8e871a877d5b35d4a57ebe1ec8bc9044eb55ba
nard-calma <nard.calma@gmail.com>
2023-02-04 19:21:25 -0800

    added handleChange and handleSubmit on sendEmail component

--- src/components/SendEmail.jsx
+++ src/components/SendEmail.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react"
+
 const SendEmail = () => {
+    const [emailContent, setEmailContent] = useState({
+        senderName: "",
+        senderEmail: "",
+        senderSubject: "",
+        senderSubject: "",
+        senderMessage: "",
+    })
+
+    const handleChange = (e) => {
+        setEmailContent({...emailContent, [e.target.name]: e.target.value})
+    }
+
+    const handleSendEmail = (e) => {
+        e.preventDefault();
+        setEmailContent({
+            senderName: "",
+            senderEmail: "",
+            senderSubject: "",
+            senderSubject: "",
+            senderMessage: "",
+        })
+    }
     return (
-        <form className="formContactMe" onSubmit={null}>
+        <form className="formContactMe" onSubmit={handleSendEmail}>
             <label htmlFor="senderName">Your Name: 
-                <input type="text" name="senderName" id="senderName" />
+                <input type="text" name="senderName" id="senderName" value={emailContent.senderName} onChange = {handleChange}/>
             </label>
             <label htmlFor="senderEmail">Your Email: 
-                <input type="text" name="senderEmail" id="senderEmail" />
+                <input type="text" name="senderEmail" id="senderEmail" value={emailContent.senderEmail} onChange = {handleChange}/>
             </label>
             <label htmlFor="senderSubject">Subject: 
-                <input type="text" name="senderSubject" id="senderSubject" />
+                <input type="text" name="senderSubject" id="senderSubject" value={emailContent.senderSubject} onChange = {handleChange}/>
             </label>
             <label className="senderMessage">Your Message: </label>
-            <textarea placeholder="enter your message here" name="senderMessage" id="senderMessage"></textarea>
+            <textarea placeholder="Enter your message here" name="senderMessage" id="senderMessage" value={emailContent.senderMessage} onChange = {handleChange}></textarea>
             <input type="submit" name="submitMessage" id="submitMessage" value="Send" />
         </form>
     )
