6736c226c540dd5738390f1893b51e345ad97b0d
nard-calma <nard.calma@gmail.com>
2023-02-07 23:57:50 -0800

    added skills state

--- src/containers/AboutMe.jsx
+++ src/containers/AboutMe.jsx
@@ -1,6 +1,83 @@
+import { useState } from "react"
 import "./AboutMe.css"
 
 const AboutMe = () => {
+    const [skillsList] = useState([
+        {
+            skill: "HTML",
+            imageLink: "https://img.icons8.com/color/256/html-5.png",
+        },
+        {
+            skill: "CSS",
+            imageLink: "https://img.icons8.com/color/256/css3.png",
+        },
+        {
+            skill: "JavaScript",
+            imageLink: "https://img.icons8.com/color/256/javascript.png",
+        },
+        {
+            skill: "C++",
+            imageLink: "https://img.icons8.com/color/256/c-plus-plus-logo.png",
+        },
+        {
+            skill: "Java",
+            imageLink: "https://img.icons8.com/color/256/java-coffee-cup-logo.png",
+        },
+        {
+            skill: "Phyton",
+            imageLink: "https://img.icons8.com/color/256/python.png",
+        },
+        {
+            skill: "React",
+            imageLink: "https://img.icons8.com/color/256/react-native.png",
+        },
+        {
+            skill: "lua",
+            imageLink: "https://img.icons8.com/nolan/256/lua-language.png",
+        },
+        {
+            skill: "PostgreSQL",
+            imageLink: "https://img.icons8.com/color/256/postgreesql.png",
+        },
+        {
+            skill: "MongoDB",
+            imageLink: "https://img.icons8.com/color/256/mongodb.png",
+        },
+        {
+            skill: "Flask",
+            imageLink: "https://img.icons8.com/color/256/flask.png",
+        },
+        {
+            skill: "NodeJS",
+            imageLink: "https://img.icons8.com/color/256/nodejs.png",
+        },
+        {
+            skill: "ExpresJS",
+            imageLink: "https://img.icons8.com/color/256/express-js.png",
+        },
+        {
+            skill: "Github",
+            imageLink: "https://img.icons8.com/color/256/github.png",
+        },
+        {
+            skill: "Bootstrap",
+            imageLink: "https://img.icons8.com/color/256/bootstrap.png",
+        },
+        {
+            skill: "Tailwind CSS",
+            imageLink: "https://img.icons8.com/color/256/tailwindcss.png",
+        },
+        {
+            skill: "Fusion360",
+            imageLink: "https://play-lh.googleusercontent.com/WVl18ugl3adNd8rjRQUJwnmt5VeiSf-7GSv7Fae8vbKywECFwsYXbD43buTZCmAR11ht",
+        },
+        {
+            skill: "Arduino",
+            imageLink: "https://img.icons8.com/color/256/arduino.png",
+        },
+    ])
+
+
     return (
         <section className="sectionAboutMe" id="idAboutMe">
                 <div className="aboutMe">
@@ -12,13 +89,6 @@ const AboutMe = () => {
                 <div className="aboutMeSkills">
                     <h1 className="skillsHeader">Skills</h1>
                     <ul className="skillsList">
-                        {/* <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/html-5.png" alt="html" /></li> */}
-                        {/* <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/css3.png" alt="css" /></li> */}
-                        <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/javascript.png" alt="javascript" /></li>
-                        {/* <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/c-plus-plus-logo.png" alt="c++" /></li> */}
-                        {/* <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/java-coffee-cup-logo.png" alt="java" /></li> */}
-                        <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/react-native.png" alt="react" /></li>
-                        <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/python.png" alt="python" /></li>
                         {/* <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/nolan/256/lua-language.png" alt="lua" /></li> */}
                         <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/postgreesql.png" alt="postgresql" /></li>
                         <li className="skillItem"><img className="skillIcon" src="https://img.icons8.com/color/256/mongodb.png" alt="mongodb" /></li>
