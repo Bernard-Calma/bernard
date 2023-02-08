8ded41a5d59147270d5fc88ded67fd652908d88c
nard-calma <nard.calma@gmail.com>
2023-02-08 00:34:02 -0800

    added style on about me span

--- src/containers/AboutMe.jsx
+++ src/containers/AboutMe.jsx
@@ -83,7 +83,7 @@ const AboutMe = () => {
                 <div className="aboutMe">
                     <h1 className = "aboutMeName">Hi! I'm Bernard Calma</h1>
                     <h2 className = "aboutMeOccupation">I'm a Software Engineer</h2>
-                    <p className="introduction">More than 1 year of experience in Full Stack Development (React, Node, Python, Flask, SQL and NoSQL) and 10 years of experience in the IT Industry(Desktop Support, Field Services) in different line of business(Telco, Retail, Media). Expert in hardware/software troubleshooting, analysis, advanced resolution procedures and complex problem solving. Skilled in Six Sigma, Cloud Computing, Team/Project Management and Automation.</p>
+                    <p className="introduction">More than 1 year of experience in Full Stack Development (<span>React</span>, <span>Node</span>, <span>Python</span>, <span>Express</span>, <span>Flask</span>, <span>SQL</span> and <span>NoSQL</span>) and 10 years of experience in the IT Industry(Desktop Support, Field Services) in different line of business(Telco, Retail, Media). Expert in hardware/software troubleshooting, analysis, advanced resolution procedures and complex problem solving. Skilled in Six Sigma, Cloud Computing, Team/Project Management and Automation.</p>
                 </div>
                 <img className="personalImage" src="images/ProfileImage.jpg" alt="Bernard Calma"/>              
                 <div className="aboutMeSkills">
