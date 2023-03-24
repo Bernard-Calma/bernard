8ed0f66ed8d10074cea24a1acf38b20d4425629e
Bernard Calma <nard.calma@gmail.com>
2023-03-24 16:47:55 -0700

    modified educational experience to be more self centered and added white space wrap for backtiks

--- src/containers/Experience.jsx
+++ src/containers/Experience.jsx
@@ -13,7 +13,10 @@ const Experience = () => {
             degree: "BS",
             yearGraduate: "(Expected Garduation: 2026)",
             schoolWebsite: "https://engineering.asu.edu/",
-            description: "- Software systems are complex, often including in excess of a million lines of code. Graduates of the BS in software engineering possess knowledge and skills of a defined engineering approach to complex systems analysis, planning, design and construction. The program has a unique, project-driven curriculum, establishing a new model of communication, teamwork, critical thinking and professionalism.",
+            description: `
+            - Learned Principles of Programing, Object Oriented Programming, Data Structures and Java.
+            - Learned System Design and construct an autonomous firefighter robot using Autodesk Fusion360 for 3D model, TinkerCAD for building circuit, Arduino for logic and MATLAB for simulations.
+            `,
             image: "images/education/ASU.png",    
             projects: [
                 "Fire Fighter Swarm - ThinkerCAD, Fusion360, Arduino, C++"
@@ -25,7 +28,10 @@ const Experience = () => {
             degree: "Certificate",
             yearGraduate: "(Graduated: 2022)",
             schoolWebsite: "https://generalassemb.ly/",
-            description: "- Software engineering immersive student in an intensive, twelve-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developing a portfolio of individual and group projects.",
+            description: `
+            - Gained 450+ hours of experience focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. 
+            - Developed a portfolio that includes individual and group projects that has been planned, designed and completed 3-4 days before deadlines.
+            `,
             image: "images/education/GA.png",    
             projects: [
                 "Auction Hall (Auction Posting App) - React Native (iOS & android), PostgreSQL, Python, Flask, Javascript, CSS StyleSheets",
@@ -41,7 +47,10 @@ const Experience = () => {
             degree: "AS",
             yearGraduate: "(Graduated: 2009)",
             schoolWebsite: "https://www.sti.edu/",
-            description: "- Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.",
+            description: `
+            - Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.
+            - Experienced building a social media application using Java and a full stack hotel reservation system using Visual Basic and Access.
+            `,
             image: "images/education/STI.png",    
             projects: [
                 "Social Media App - Java, NetBeans",
