ab080f7d92441d809ad65b858a957ff4d20d04ce
Bernard Calma <nard.calma@gmail.com>
2023-03-27 01:06:34 -0700

    added project details div and positioned it in the middle of the screen

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,17 @@
 const Project = (props) => {
     return <div className="containerProject">
         <img className="projectImage" src={props.project.screenshot} alt={props.project.name}/>
+        <div className="projectDetails">
+            <img className="projectImageLarge" src={props.project.screenshot} alt={props.project.name}/>
+            <h2 className={`projectTitle`}>{props.project.name}</h2>
+            <div className="containerImage">
+                <a href={props.project.websiteLink} target={'_blank'} rel={'noreferrer'} className="projectLink">Link Here</a>
+            </div>
+            <p className="projectSummary">{props.project.summary}</p>
+            <ul className="skillList">
+                {props.project.skills.map((skill, index) => <li className="skillItem" key={index}>{skill}</li>)}
+            </ul>
+        </div>
     </div>
 }
 
